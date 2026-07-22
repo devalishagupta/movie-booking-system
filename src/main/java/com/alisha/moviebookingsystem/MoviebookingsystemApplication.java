5d15637760a6cb29cc8f51d100aaea9c6b1f1d62
Alisha Gupta <g0707.alisha@gmail.com>
2026-07-22 15:15:32 +0530

    chore: bootstrap Spring Boot Project

---
+++ src/main/java/com/alisha/moviebookingsystem/MoviebookingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.alisha.moviebookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviebookingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviebookingsystemApplication.class, args);
+	}
+
+}
